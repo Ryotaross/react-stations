09209857e9ddfdc8830559825f8423d0a8f43229
Ryotaro Shimizu <ryotaro_625@icloud.com>
2021-09-15 17:19:04 +0900

    20210915 1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 // DO NOT DELETE
 
 import * as React from 'react'
+import { useState,useEffect } from 'react'
+import { render } from 'react';
 import './App.css'
 
 /**
@@ -8,9 +10,34 @@ import './App.css'
  * @type {React.FC}
  */
 export const App = () => {
+  const [dogUrl,setDogUrl] = useState("https://images.dog.ceo/breeds/terrier-scottish/n02097298_91.jpg");
+
+const handleClick = () => {
+  fetch('https://dog.ceo/api/breeds/image/random')
+    .then(res => res.json())
+    .then(
+      (result) => {
+        const dog = result.message
+        setDogUrl(dog);
+      },
+      (error) => {
+      }
+    )
+}
+
   return (
     <div>
-      <h2>Hello, world!</h2>
+      <header className="top">
+        Dogアプリ
+      </header>
+      <div className="clearfix main">
+        <p className="leftMenu">犬の画像を表示するサイトです</p>
+        <div className="rightMenu">
+          <img className="photo" src={dogUrl}></img>
+          <button className="btn" onClick={handleClick}>更新</button>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
+
